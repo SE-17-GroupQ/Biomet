bbe727ca4c9b9f5b2ce87f910b2c295493f945e6
Harshal Gala <harshalbgala@gmail.com>
2017-03-13 10:54:12 -0400

    Minor Change in launch camera

--- app/src/main/java/com/example/gala/biomet/ApiRelated.java
+++ app/src/main/java/com/example/gala/biomet/ApiRelated.java
@@ -26,7 +26,8 @@ import okhttp3.Response;
  * Created by Gala on 3/6/2017.
  */
 
-public class ApiRelated extends AsyncTask<String, String, String>{
+
+public class ApiRelated extends AsyncTask<String, String, String> {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -43,84 +44,86 @@ public class ApiRelated extends AsyncTask<String, String, String>{
     private static final String CONNECTION_PROBLEM = "Connection problem. Please try again.";
 
     //Kaisros Api Constants
-    static final String API_LINK = "https://api.kairos.com";
-    private static final String IMAGE = "image";
-    private static final String GALLERY_NAME = "gallery_name";
-    private static final String GALLERY_VAL = "se17";
+    private static final String API_LINK = "https://api.kairos.com";
+    public static final String RECOGNIZE = "recognize";
     private static final String ENROLL = "enroll";
-    private static final String SUBJECT_ID = "subject_id";
-    private static final String APP_ID_VALUE = "01ed2a33";
-    private static final String APP_KEY_VALUE = "b6043be30810c2de07daefe36e89cb20";
     private static final String APP_ID = "app_id";
     private static final String APP_KEY = "app_key";
+    private static final String IMAGE = "image";
+    private static final String SUBJECT_ID = "subject_id";
+    private static final String GALLERY_NAME = "gallery_name";
+    private static final String IMAGES = "images";
     private static final String ERRORS = "Errors";
     private static final String MESSAGE = "Message";
     private static final String TRANSACTION = "transaction";
     private static final String STATUS = "status";
-    private static final String IMAGES_JSON = "images";
+    public static final String SUCCESS = "success";
+    public static final String MESSAGE_FAILURE = "message";
+
+    private static final String GALLERY_VAL = "se17";
+    private static final String APP_ID_VALUE = "01ed2a33";
+    private static final String APP_KEY_VALUE = "b6043be30810c2de07daefe36e89cb20";
+
     //Kaisros Api Constants
 
     private static final String EMPTY_UNITY_ID = "Unity Id cannot be blank";
     private String subject_id;
-    private String apiService;
-    private EditText subjectText;
+    //private String apiService;
+    //private EditText subjectText;
     private TextView status;
+
 
     protected String doInBackground(String... params) {
         //This is the return message in the end of this message
         String returnMsg = "";
-        //
         String base64Photo = params[0];
         JSONObject jsonObject = new JSONObject();
-        Log.i(TAG, "Kairos API call :\nSERVICE : "+apiService+" GALLERY : "+GALLERY_VAL);
+        Log.i(TAG, "API call :\nSERVICE : " + MainActivity.apiService + " \n GALLERY : " + GALLERY_VAL);
         try {
             jsonObject.putOpt(IMAGE, base64Photo);
-            jsonObject.putOpt(GALLERY_NAME,GALLERY_VAL);
-            if(apiService.equals(ENROLL)){
-                Log.i(TAG, "SUBJECT : "+subject_id);
+            jsonObject.putOpt(GALLERY_NAME, GALLERY_VAL);
+            if (MainActivity.apiService.equals(ENROLL)) {
+                Log.i(TAG, "SUBJECT : " + subject_id);
                 jsonObject.putOpt(SUBJECT_ID, subject_id);
             }
-        }
-        catch(JSONException jerror){
+        } catch (JSONException jerror) {
             Log.i(TAG, "Problem in adding parameters to JSON");
             jerror.printStackTrace();
         }
-        RequestBody requestBody = RequestBody.create(JSON,jsonObject.toString());
-        Request request = new Request.Builder().url(API_LINK +"/"+apiService).
-                addHeader(APP_ID,APP_ID_VALUE).addHeader(APP_KEY,APP_KEY_VALUE)
-                .addHeader(CONTENT,CONTENT_TYPE).post(requestBody).build();
+        RequestBody requestBody = RequestBody.create(JSON, jsonObject.toString());
+        Request request = new Request.Builder().url(API_LINK + "/" + MainActivity.apiService).
+                addHeader(APP_ID, APP_ID_VALUE).addHeader(APP_KEY, APP_KEY_VALUE)
+                .addHeader(CONTENT, CONTENT_TYPE).post(requestBody).build();
 
         Response response = null;
-        try{
+        try {
             response = client.newCall(request).execute();
-            if(response != null && response.isSuccessful()) {
+            if (response != null && response.isSuccessful()) {
                 Log.i(TAG, "Response successful");
                 String responseJsonString = response.body().string();
-                try{
+                try {
                     JSONObject responseJson = new JSONObject(responseJsonString);
-                    Log.i(TAG, "Response JSON : "+responseJsonString);
-                    if(responseJson.has(ERRORS)) {
-                        String apiError = ((JSONObject)responseJson.getJSONArray(ERRORS).get(0)).getString(MESSAGE);
-                        Log.i(TAG, "Kairos Error : "+apiError);
-                        returnMsg = apiService+" : "+apiError;
-                    }
-                    else {
-                        JSONObject transaction =((JSONObject)responseJson.getJSONArray(IMAGES_JSON).get(0))
+                    Log.i(TAG, "Response JSON : " + responseJsonString);
+                    if (responseJson.has(ERRORS)) {
+                        String apiError = ((JSONObject) responseJson.getJSONArray(ERRORS).get(0)).getString(MESSAGE);
+                        Log.i(TAG, "Kairos Error : " + apiError);
+                        returnMsg = MainActivity.apiService + " : " + apiError;
+                    } else {
+                        JSONObject transaction = ((JSONObject) responseJson.getJSONArray(IMAGES).get(0))
                                 .getJSONObject(TRANSACTION);
                         String kairosStatus = transaction.getString(STATUS);
                         String subject = transaction.getString(SUBJECT_ID);
-                        Log.i(TAG, "Kairos Status : "+kairosStatus);
-                        returnMsg = apiService+" "+subject+" : "+kairosStatus;
+                        Log.i(TAG, "Kairos Status : " + kairosStatus);
+                        returnMsg = MainActivity.apiService + " " + subject + " : " + kairosStatus;
                     }
-                } catch(JSONException j) {
+                } catch (JSONException j) {
                     Log.i(TAG, "Problem in parsing response JSON");
                     j.printStackTrace();
                 }
+            } else {
+                Log.i(TAG, "Response failed : " + response.code());
             }
-            else {
-                Log.i(TAG, "Response failed : "+response.code());
-            }
-        } catch(IOException i) {
+        } catch (IOException i) {
             Log.i(TAG, "Problem in :\n1. Executing Kairon POST request\nOR\n2.Getting body of response");
             returnMsg = CONNECTION_PROBLEM;
             i.printStackTrace();
@@ -128,17 +131,19 @@ public class ApiRelated extends AsyncTask<String, String, String>{
 
         return returnMsg;
     }
+
     @Override
     protected void onPostExecute(String s) {
-        subjectText.setText("");
+        /*MainActivity.subjectText.setText("");
         status.setText(s);
-        super.onPostExecute(s);
+        super.onPostExecute(s);*/
     }
 
-    public void clickPhoto(View v)
+}
+    /*public void clickPhoto(View v)
     {
-        apiService = ((Button)v).getText().toString().toLowerCase();
-        if(apiService.equals(ENROLL))
+        MainActivity.apiService = ((Button)v).getText().toString().toLowerCase();
+        if(MainActivity.apiService.equals(ENROLL))
         {
             subject_id = subjectText.getText().toString();
             if(subject_id.matches(""))
@@ -149,6 +154,4 @@ public class ApiRelated extends AsyncTask<String, String, String>{
             }
             Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         }
-    }
-
-}
+    }*/
--- app/src/main/java/com/example/gala/biomet/MainActivity.java
+++ app/src/main/java/com/example/gala/biomet/MainActivity.java
@@ -17,32 +17,20 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-
 import java.io.ByteArrayOutputStream;
-
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    private final OkHttpClient client = new OkHttpClient();
-    private final Gson gson = new Gson();
-    static final String API_LINK = "https://api.kairos.com";
-    private static final String IMAGE = "image";
-    private static final String GALLERY_NAME = "gallery_name";
-    private static final String GALLERY_VAL = "se17";
-    private static final String REGISTER_STUDENT = "register student";
 
+    private static final String ENROLL = "enroll";
 
     private static final String EMPTY_UNITY_ID = "Unity Id cannot be blank";
     private ImageView imageView;
-    private String apiService;
+    public static String apiService;
     private Bitmap bitmapPhoto;
     private TextView status;
-    private EditText subjectText;
+    public static EditText subjectText;
     private String subject_id;
 
     private AlertDialog alertDialog;
@@ -57,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button markAttendanceButton = (Button) findViewById(R.id.markAttendanceButton);
         Button registerButton = (Button) findViewById(R.id.registerButton);
+        Button captureImage = (Button) findViewById(R.id.captureImage);
 
         imageView = (ImageView) findViewById(R.id.imageView);
         //subjectText = (EditText) findViewById(R.id.unity_id);
@@ -77,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.w(TAG,apiService);
                 subjectText = (EditText) dialog.findViewById(R.id.unity_id);
 
-                if(apiService.equals(REGISTER_STUDENT))
+                if(apiService.equals(ENROLL))
                 {
                     Log.w(TAG,"5");
 
@@ -89,17 +78,6 @@ public class MainActivity extends AppCompatActivity {
                         Log.i(TAG, "not null, silver");
                     }
                     Log.w(TAG,"7");
-                    //Log.w(TAG,subject_id);
-                    /*if(subject_id.matches(""))
-                    {
-                        Log.w(TAG,"ealfjjbwefkjwjbs");
-                        alertDialog.setMessage(EMPTY_UNITY_ID);
-                        alertDialog.show();
-                        return;
-                    }*/
-                    Log.w(TAG,"8");
-                    /*Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(photoIntent,REQUEST_IMAGE_CAPTURE);*/
                 }
                 Button dialogButtonClose = (Button)dialog.findViewById(R.id.popupClose);
                 dialogButtonClose.setOnClickListener(new View.OnClickListener() {
@@ -108,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
+                Log.w(TAG,"before dialog");
                 dialog.show();
             }
         });
@@ -127,18 +106,21 @@ public class MainActivity extends AppCompatActivity {
     //Launching the camera
     public void launchCamera(View view) {
         Log.i(TAG, ((Button)view).getText().toString());
-
-        Log.i(TAG, subjectText.getText().toString());
+        if(subject_id.matches("")) {
+            Log.i(TAG, "not null, silver");
+            Log.i(TAG, subjectText.getText().toString());
+        }
 
         subject_id = subjectText.getText().toString();
-        if(subject_id.matches(""))
-        {
-            Log.w(TAG,"ealfjjbwefkjwjbs");
-            alertDialog.setMessage(EMPTY_UNITY_ID);
-            alertDialog.show();
-            return;
+        if(apiService.matches(ENROLL)) {
+            if (subject_id.matches("")) {
+                Log.w(TAG, "ealfjjbwefkjwjbs");
+                alertDialog.setMessage(EMPTY_UNITY_ID);
+                alertDialog.show();
+                return;
+            }
+            Log.i(TAG, subject_id);
         }
-        Log.i(TAG,subject_id);
         apiService = ((Button) view).getText().toString().toLowerCase();
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //Take a picture and pass results along to onActivityResults
@@ -153,12 +135,26 @@ public class MainActivity extends AppCompatActivity {
             //Get the photo
             Bundle extras = data.getExtras();
             Bitmap photo = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(photo);//Just for reference
+            //imageView.setImageBitmap(photo);//Just for reference
             bitmapPhoto = photo;
 
             String base64Photo = bitmapToBase64(photo);
-        }
 
+            try {
+                subjectText.setText("");
+                alertDialog.setMessage(apiService + " in progress..");
+                /*for(Button b : buttons) {
+                    b.setEnabled(false);
+                }*/
+                Log.w(TAG,"before alert dialog");
+                alertDialog.show();
+                new ApiRelated().execute(base64Photo);
+            } catch(Exception e) {
+                Log.i(TAG, "ApiRelated problem : "+e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
     //Conversion from Bitmap to Base64
     private String bitmapToBase64(Bitmap photo) {
@@ -167,4 +163,5 @@ public class MainActivity extends AppCompatActivity {
         byte[] byteArray = byteArrayOutputStream.toByteArray();
         return Base64.encodeToString(byteArray, Base64.DEFAULT);
     }
+
 }
